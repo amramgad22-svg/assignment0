5a3f48dc2ad680949fd8e08afa00b0c12dcf3266
Amr Hassanien <amr.hassanien@student.giu-uni.de>
2026-09-26 00:22:52 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "Amr the Mage";
+const studentId = 16002795;
+const power = studentId * 6;
+const warcry = "By the Delta marshes, the Stone Golem flees!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
